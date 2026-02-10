696eb022bbcfcc1cafccee78930ac1c041a09100
Walker1402 <89437098+Walker1402@users.noreply.github.com>
2026-02-10 22:04:37 +0530

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -7,6 +7,7 @@ ollama pull gemma3:4b
 ollama pull qwen3-vl:4b
 ollama pull phi4-mini-reasoning:3.8b-q4_K_M
 
-echo Setup Complete! Run the app with: MacroMoEStudio.py
-
+echo Setup Complete!
+echo Starting Application...
+python MacroMoEStudio.py
 pause
